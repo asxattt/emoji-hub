37975c322df2fb2e1c59f37ca608e15cfe597d4c
asxattt <askhat11@gmail.com>
2025-09-09 21:58:37 +0500

    Fix Flask host and port for Railway

--- app.py
+++ app.py
@@ -35,5 +35,7 @@ def emojis():
 
     return render_template("emojis.html", emojis=emojis)
 
+import os
 if __name__=="__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port,debug=True)
